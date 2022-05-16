b20f732704eb9aef81485602c18b23568b9cb0c6
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-05-16 17:52:29 +0600

    connect success to MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -6,6 +7,24 @@ const port = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbuwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        console.log('MongoDB Connected');
+        // const collection = client.db('doctors-portal').collection('')
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir)
+
+
 
 app.get('/', (req, res) => {
     res.send('Doctors Portal')
